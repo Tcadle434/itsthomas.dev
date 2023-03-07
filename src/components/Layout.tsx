37f074097679b9108cb45b09965bd4a63d44f1f5
Tcadle434 <thomascadle434@gmail.com>
2023-03-07 16:50:33 -0700

    fixed hydration error

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -30,17 +30,17 @@ export default function Layout({
         <meta name="theme-color" content="#27272a" />
         <meta property="og:site_name" content="Thomas's Portfolio" />
       </Head>
-      <body className="flex max-w-3xl flex-col bg-zinc-900 antialiased md:mt-8 md:flex-row lg:mx-auto lg:mt-12">
+      <main className="flex max-w-3xl flex-col bg-zinc-900 antialiased md:mt-8 md:flex-row lg:mx-auto lg:mt-12">
         <div className="fixed left-0 z-50 hidden h-full w-[6%] md:block lg:block">
           <NavBar path={currentRoute} />
         </div>
         <div className="fixed top-0 z-50 block w-full px-8 pt-4 md:hidden lg:hidden">
           <MobileNavBar path={currentRoute} />
         </div>
-        <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:mt-0 md:px-0">
+        <div className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:mt-0 md:px-0">
           {children}
-        </main>
-      </body>
+        </div>
+      </main>
     </div>
   );
 }
